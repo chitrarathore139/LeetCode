cc3d4a42961bf36f08281c5c55f851960907a5e4
Chitra Rathore <133988193+chitrarathore139@users.noreply.github.com>
2026-08-20 22:58:40 +0530

    Time: 926 ms (52.40%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1541-top-travellers/top-travellers.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT Users.name AS name,
+       COALESCE(SUM(Rides.distance), 0) AS travelled_distance
+FROM Users
+LEFT JOIN Rides
+ON Users.id = Rides.user_id
+GROUP BY Users.id, Users.name
+ORDER BY travelled_distance DESC,
+         Users.name ASC;
