208d1e0c3b7a6f10886b00fec7087185cb1d2a19
Chitra Rathore <133988193+chitrarathore139@users.noreply.github.com>
2026-09-25 18:09:53 +0530

    Time: 451 ms (98.53%) | Memory: 0B (100.00%) - LeetSync

--- 1179-game-play-analysis-i/game-play-analysis-i.sql
+++ 1179-game-play-analysis-i/game-play-analysis-i.sql
@@ -1,4 +1,1 @@
-SELECT a.player_id,
-       MIN(a.event_date) AS first_login
-FROM Activity a
-GROUP BY a.player_id;
+SELECT a.player_id, MIN(a.event_date) AS first_login FROM Activity a GROUP BY a.player_id;
